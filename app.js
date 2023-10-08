819e5a5ea3779e559864866bc2a7ce0897ad5659
Nanahawaw <folarinyetunde@gmail.com>
2023-10-08 13:31:54 +0100

    added javascript to toggle active class across pages

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let currentPath = window.location.pathname;
+  let navLinks = document.querySelectorAll(".navbar-nav .nav-item .nav-link");
+  navLinks.forEach(function (link) {
+    link.classList.remove("active");
+  });
+  //add active class
+  navLinks.forEach(function (link) {
+    if (currentPath.endsWith(link.getAttribute("href"))) {
+      link.classList.add("active");
+    }
+  });
+});
